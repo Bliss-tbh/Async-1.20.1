c81e9a1d9fb3c2b2322d439df282bdb48a3511c8
Cubic <132150361+MrCubicYT@users.noreply.github.com>
2025-01-16 22:26:03 +0000

    fix

--- fabric/src/main/java/com/axalotl/async/mixin/utils/FastUtilsMixin.java
+++ fabric/src/main/java/com/axalotl/async/mixin/utils/FastUtilsMixin.java
@@ -4,14 +4,12 @@ import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.longs.Long2LongOpenHashMap;
 import it.unimi.dsi.fastutil.longs.Long2ObjectLinkedOpenHashMap;
-import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.longs.LongLinkedOpenHashSet;
 import it.unimi.dsi.fastutil.objects.*;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(value = {
         Int2ObjectOpenHashMap.class,
-        Long2ObjectOpenHashMap.class,
         LongLinkedOpenHashSet.class,
         ObjectOpenCustomHashSet.class,
         Long2LongOpenHashMap.class,
@@ -24,22 +22,35 @@ import org.spongepowered.asm.mixin.Mixin;
         Reference2IntOpenHashMap.class,
         ReferenceArrayList.class,
         Object2ReferenceOpenCustomHashMap.class,
+        Object2ReferenceOpenCustomHashMap.class,
         Reference2ByteOpenHashMap.class,
         Reference2LongOpenHashMap.class
 },
         targets = {
                 "it.unimi.dsi.fastutil.longs.Long2LongOpenHashMap$FastEntryIterator",
+                "it.unimi.dsi.fastutil.longs.LongLinkedOpenHashSet",
+                "it.unimi.dsi.fastutil.objects.ObjectOpenCustomHashSet",
                 "it.unimi.dsi.fastutil.longs.Long2LongOpenHashMap$MapIterator",
+                "it.unimi.dsi.fastutil.longs.Long2ObjectLinkedOpenHashMap",
+                "it.unimi.dsi.fastutil.objects.ReferenceArrayList",
+                "it.unimi.dsi.fastutil.objects.Object2ReferenceOpenCustomHashMap",
+                "it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap",
+                "it.unimi.dsi.fastutil.ints.IntArrayList",
+                "it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap",
                 "it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap$MapIterator",
+                "it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap",
+                "it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet",
+                "it.unimi.dsi.fastutil.objects.Reference2IntOpenHashMap",
+                "it.unimi.dsi.fastutil.objects.Reference2ReferenceArrayMap",
                 "it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap$ValueIterator",
                 "it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap$KeySet",
                 "it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap$KeyIterator",
                 "it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap$MapEntrySet",
                 "it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap$EntryIterator",
                 "it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap$MapIterator",
-                "it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap$MapIterator"
+                "it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap$MapIterator",
+                "it.unimi.dsi.fastutil.objects.Reference2ByteOpenHashMap",
+                "it.unimi.dsi.fastutil.objects.Reference2LongOpenHashMap"
         }, priority = 50000)
-public abstract class FastUtilsMixin {
+public class FastUtilsMixin {
 }
-
-
