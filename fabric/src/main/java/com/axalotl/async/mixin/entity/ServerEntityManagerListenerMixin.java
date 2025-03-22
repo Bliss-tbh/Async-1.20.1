e31e2909c99a6a6fb7a617522a232b86120455dd
alchemyyy <github@grassmail.net>
2025-03-22 12:52:29 -0700

    initial backport to forge/neoforge 1.20.1

--- fabric/src/main/java/com/axalotl/async/mixin/entity/ServerEntityManagerListenerMixin.java
+++ fabric/src/main/java/com/axalotl/async/mixin/entity/ServerEntityManagerListenerMixin.java
@@ -2,26 +2,26 @@ package com.axalotl.async.mixin.entity;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import net.minecraft.entity.Entity;
-import net.minecraft.server.world.ServerEntityManager;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.entity.PersistentEntitySectionManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
 import java.util.concurrent.locks.ReentrantLock;
 
-@Mixin(ServerEntityManager.Listener.class)
+@Mixin(PersistentEntitySectionManager.Callback.class)
 public abstract class ServerEntityManagerListenerMixin implements AutoCloseable {
     @Unique
     private static final ReentrantLock lock = new ReentrantLock();
 
-    @WrapMethod(method = "updateEntityPosition")
+    @WrapMethod(method = "onMove")
     private void updateEntityPosition(Operation<Void> original) {
         synchronized (lock) {
             original.call();
         }
     }
 
-    @WrapMethod(method = "remove")
+    @WrapMethod(method = "onRemove")
     private void remove(Entity.RemovalReason reason, Operation<Void> original) {
         synchronized (lock) {
             original.call(reason);
